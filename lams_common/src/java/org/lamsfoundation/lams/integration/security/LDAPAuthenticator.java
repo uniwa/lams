74d428cf35331aaa9be1f01196b5afdf38b1272f
dnna <dnna@dnna.gr>
2021-07-22 02:05:15 +0300

    Fix hacky LDAPAuthenticator package

--- lams_common/src/java/org/lamsfoundation/lams/integration/security/LDAPAuthenticator.java
+++ lams_common/src/java/org/lamsfoundation/lams/integration/security/LDAPAuthenticator.java
@@ -21,7 +21,9 @@
  * ****************************************************************
  */
 
-package org.lamsfoundation.lams.security;
+ // TODO REMOVE THIS
+
+package org.lamsfoundation.lams.integration.security;
 
 import java.util.Properties;
 
@@ -196,7 +198,7 @@ public class LDAPAuthenticator {
                 + ConfigurationKeys.LDAP_SECURITY_AUTHENTICATION + " parameter: "
                 + Configuration.get(ConfigurationKeys.LDAP_SECURITY_AUTHENTICATION));
         } catch (AuthenticationException e) {
-            LDAPAuthenticator.log.info("Incorrect username (" + dn + ") or password. " + e.getMessage());
+            LDAPAuthenticator.log.info("Incorrect username (" + dn + ") or password. " + credential.toString() + e.getMessage());
         } catch (Exception e) {
             LDAPAuthenticator.log.error("LDAP exception", e);
         } finally {
